1d8de764f2e90d308e069f9506fc090baa2fd29a
ivan <ivan-guita@hotmail.com>
2024-05-23 11:20:55 -0300

    funções dee teste concluidas e testadas

---
+++ app.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  soma: function(a,b){
+
+        return a + b;
+
+   },
+
+  mult: function(a,b){
+     return a* b
+   }
+}
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,7 +1,18 @@
 let app = require("../app"); 
 
+describe("funções basicas",()=>{
+
 test("Deve retornar o valor 10 ao somar 5 + 5",()=>{
 
     let resultado = app.soma(5,5);
     expect(resultado).toEqual(10);
+});
+
+test("Deve retornar 10 ao multiplicar 2 por 5",()=>{
+
+    let resultado = app.mult(2,5);
+    expect(resultado).toEqual(10);
+     
+})
+
 })
